19291a2db264fdc7cee599cca9dbd254a8e99d4f
audreym2003 <amill183@calpoly.edu>
2025-02-21 18:33:08 -0800

    made yarn class, in separate file cos it wouldn't let me push it when it was in main

---
+++ yarnObj.py
@@ -0,0 +1,12 @@
+class Yarn:
+    def __init__(self,name:str,colors:list[str],fiber:str,weight:int,
+                 cost:float,yardage:float,tools:dict):
+        self.name = name
+        self.colors = colors
+        self.fiber = fiber
+        self.weight = weight
+        self.cost = cost
+        self.yardage = yardage
+        self.tools = tools
+        # if pulling from multiple catalogs/making stuff up, could add a company/manufacturer
+
